c0115d5cef213ea473a86b120d7819d466f5376c
d34db3ff <the@stella.observer>
2025-01-31 10:21:09 +0100

    bugfix: escape .plaintext properly

--- import-data.sh
+++ import-data.sh
@@ -5,7 +5,8 @@ if [ ! -d ./tmp ]; then
 fi
 
 cat $1 | jq -cr '.db[0].data.posts | map(select(.status == "published"))[] | .slug' > tmp/keys
-cat $1 | jq -cr '.db[0].data.posts | map(select(.status == "published"))[] | {"page": {"meta_title": .title, "body_class": (if .type == "post" then "post-template" else "page-template" end)}, "post": {"post_class": "post no-image", "title": .title, "plaintext": (if .plaintext != null then .plaintext | gsub("\\n"; "") else null end), "html": (if .html == null then "" else .html | @json | ltrimstr("\"") |  rtrimstr("\"") end)}}' > ./tmp/values
+cat $1 | jq -cr '.db[0].data.posts | map(select(.status == "published"))[] | {"page": {"meta_title": .title, "body_class": (if .type == "post" then "post-template" else "page-template" end)}, "post": {"post_class": "post no-image", "title": .title, "plaintext": (if .plaintext != null then .plaintext | @json | ltrimstr("\"") |  rtrimstr("\"") | gsub("\\n"; "") else null end), "html": (if .html == null then "" else .html | @json | ltrimstr("\"") |  rtrimstr("\"") end)}}' > ./tmp/values
+
 while read key <&3 && read value <&4; do
   bunx wrangler kv key put "/${key}/" "$value" --binding=DATA --local
 done 3<tmp/keys 4<tmp/values
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ interface Env {
 }
 
 async function router(path: string, data: KVNamespace, settings: KVNamespace) {
-	if (path === '/' || path.startsWith("/page/")) {
+	if (path === '/' || path.startsWith('/page/')) {
 		return handle_index(path, data, settings);
 	} else if (path === '/portal/' || path === '/about/') {
 		// TODO: find a better way to distinguish pages from posts.
@@ -21,22 +21,23 @@ async function router(path: string, data: KVNamespace, settings: KVNamespace) {
 }
 
 async function handle_index(path: string, data: KVNamespace, settings: KVNamespace) {
-	const site = JSON.parse(await settings.get("site") || '{}');
+	const site = JSON.parse(await settings.get('site') || '{}');
 	const navigation = JSON.parse(await settings.get("navigation") || '{}');
-	const posts = JSON.parse(await settings.get("posts") || '{}').slice(0, 1);
+	const posts = JSON.parse(await settings.get('posts') || '{}').slice(0, 5);
 	let page_data;
 	if (path === '/') {
 		page_data = JSON.parse(await data.get(path) || '{}');
 	} else {
 		page_data = JSON.parse(await data.get(path) || '{}');
+		page_data.body_class = '';
 		// TODO: handle pagination
 	}
 	return Handlebars.templates['index.hbs']({ "page": page_data.page, posts, site, navigation });
 }
 
 async function handle_page(path: string, data: KVNamespace, settings: KVNamespace) {
-	const site = JSON.parse(await settings.get("site") || '{}');
-	const navigation = JSON.parse(await settings.get("navigation") || '{}');
+	const site = JSON.parse(await settings.get('site') || '{}');
+	const navigation = JSON.parse(await settings.get('navigation') || '{}');
 	const page_data = JSON.parse(await data.get(path) || '{}');
 	const post = page_data.post;
 	const page = page_data.page;
